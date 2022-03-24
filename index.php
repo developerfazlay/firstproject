c5c59583a8ddff4521522b6cbc57dfbe667ac883
developerfazlay <102307581+developerfazlay@users.noreply.github.com>
2022-03-24 21:07:18 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+ /*I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk. */
+
+ $monitor_quantity = 3; //Integer Datatype
+ $model = "hp monitor"; //String Datatype
+ $monitor_condition = "old"; //String Datatype
+ $display_size = 15.5; //Double/Float Datatype
+ $vga_cable_status = True; //Boolean Datatype
+ $price = 8000; //Integer Datatype
+
+
+?>
+
+
+</body>
+</html>
